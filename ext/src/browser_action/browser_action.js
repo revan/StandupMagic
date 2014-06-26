87cb1600b12ce38e3c32d9b95786f63e70b9ba5a
Revan Sopher <rsopher@gmail.com>
2014-06-26 18:10:22 -0400

    Cleanup, copy css.

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -6,66 +6,6 @@ window.addEventListener('load', function() {
   Asana.ServerModel.onError = function(response) {
     showError(response.errors[0].message);
   };
-
-  // Ah, the joys of asynchronous programming.
-  // To initialize, we've got to gather various bits of information.
-  // Starting with a reference to the window and tab that were active when
-  // the popup was opened ...
-  chrome.windows.getCurrent(function(w) {
-    chrome.tabs.query({
-      active: true,
-      windowId: w.id
-    }, function(tabs) {
-      // Now load our options ...
-      Asana.ServerModel.options(function(options) {
-        // And ensure the user is logged in ...
-        Asana.ServerModel.isLoggedIn(function(is_logged_in) {
-          if (is_logged_in) {
-            if (window.quick_add_request) {
-              // If this was a QuickAdd request (set by the code popping up
-              // the window in Asana.ExtensionServer), then we have all the
-              // info we need and should show the add UI right away.
-              showAddUi(
-                  quick_add_request.url, quick_add_request.title,
-                  quick_add_request.selected_text, options);
-            } else {
-              // Otherwise we want to get the selection from the tab that
-              // was active when we were opened. So we set up a listener
-              // to listen for the selection send event from the content
-              // window ...
-              var selection = "";
-              var listener = function(request, sender, sendResponse) {
-                if (request.type === "selection") {
-                  chrome.extension.onRequest.removeListener(listener);
-                  console.info("Asana popup got selection");
-                  selection = "\n" + request.value;
-                }
-              };
-              chrome.extension.onRequest.addListener(listener);
-
-              // ... and then we make a request to the content window to
-              // send us the selection.
-              var tab = tabs[0];
-              chrome.tabs.executeScript(tab.id, {
-                code: "(Asana && Asana.SelectionClient) ? Asana.SelectionClient.sendSelection() : 0"
-              }, function() {
-                fetchYesterday(options);
-                // The requests appear to be handled synchronously, so the
-                // selection should have been sent by the time we get this
-                // completion callback. If the timing ever changes, however,
-                // that could break and we would never show the add UI.
-                // So this could be made more robust.
-                showAddUi(tab.url, tab.title, selection, options);
-              });
-            }
-          } else {
-            // The user is not even logged in. Prompt them to do so!
-            showLogin(Asana.Options.loginUrl(options));
-          }
-        });
-      });
-    });
-  });
 });
 
 var appendEach = function(list, id){
